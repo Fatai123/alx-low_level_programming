99851c5e72749dd0f28c7df14d3dd0f085f2350c
Fatai123 <fataioyetunji34@gmail.com>
2022-03-11 02:26:50 -0800

    C - Hello, World

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - program that prints exactly "with proper grammar, but the outcome is a piece of art"
+ * Return: 0 if properly exited
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
