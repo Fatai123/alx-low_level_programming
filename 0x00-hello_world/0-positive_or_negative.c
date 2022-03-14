5feb7f1870bd5f93ae119fdb82f2f076168998b8
Fatai123 <fataioyetunji34@gmail.com>
2022-03-14 14:49:14 -0700

    Description of projects

---
+++ 0x00-hello_world/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - print the random number assigned to the variable n 
+ * each time it is executed
+ * Return: Aways 0 (Success/ correct)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf ("%d is positive\n", n);
+	else if (n == 0)
+		printf ("%d is zero\n", n);
+	else if (n < 0)
+		printf ("%d is negetive\n, n");
+	return (0);
+}
+
