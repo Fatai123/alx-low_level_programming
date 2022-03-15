7e8ee6165a2530b0d23be2f94ab9212d12e12b79
Fatai123 <fataioyetunji34@gmail.com>
2022-03-14 17:23:49 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,15 +5,13 @@
  * followed by a new line.                                                                                                   
  * Return: Always 0 (Success/correct)                                                                                        
  */                                                                                                                          
-int main(void)                                                                                                               
-{                                                                                                                          
-       	
-	char alphabet;		                                                                                                                            
-      	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)                                                                   
-		putchar (alphabet);                                                                                         
+int main(void)                                                                                                               {
+	int alphabet;
+	
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		putchar (alphabet);
 	for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
 		putchar (alphabet);
-
 	putchar ('\n');
 	return (0);                                                                                                                  
 }               
