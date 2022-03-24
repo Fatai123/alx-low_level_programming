96001be3e8f8c427309e2ab39717c2c79d89b447
Fatai123 <fataioyetunji34@gmail.com>
2022-03-24 08:02:47 -0700

    do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "main"
+
+/**
+ * _puts - To print a string, followed by a new to stdout
+ *
+ * @str: pointer to the string
+ */
+void _puts(char *str)
+{
+	int i;
+
+	for(i = 0, str[i] != '\0', i++)
+	{
+		_putchar(str[i]);
+	}
+
+	_putchar ('\n');
+}
