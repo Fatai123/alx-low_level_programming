86f644ab48944ab2f785eea3e9738c41e46516dd
Fatai123 <fataioyetunji34@gmail.com>
2022-03-26 07:14:48 -0700

    Compea two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcmp - Function that compares tow strings.
+ * @s1: first string to compare
+ * @s2: second string to compare
+ *
+ * Return: 0 if s1 and s2 are the same; less than
+ * 0 if s1<s2; greater than 0 if s1>s2.
+ */
+int _strcmp(char *s1, char *s2);
+{
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+
+		s1++;
+		s2++;
+
+	}
+	return (*s1 - *s2);
+}
