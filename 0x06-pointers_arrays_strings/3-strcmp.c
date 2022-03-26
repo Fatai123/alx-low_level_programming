705ba47f62515222a5993352ced71bbc63f26705
Fatai123 <fataioyetunji34@gmail.com>
2022-03-26 07:28:53 -0700

    Compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,7 +8,7 @@
  * Return: 0 if s1 and s2 are the same; less than
  * 0 if s1<s2; greater than 0 if s1>s2.
  */
-int _strcmp(char *s1, char *s2);
+int _strcmp(char *s1, char *s2)
 {
 	while (*s1 == *s2)
 	{
